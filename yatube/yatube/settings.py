0a720a63c4ab93348c4c8da7abc13881d4202512
Nikita Korovyakovskiy@mail.ru <nikita.korovyakovskiy@mail.ru>
2022-11-07 21:10:29 +0700

    2try

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fa2%ny%3(4cr29=emy87jjeewrh6_%zh#$cy7&0#y^th2w5h2)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
